a9f381444a67ec238f02f4d55f7e9f552679740f
Saif Ullah <104705537+sunnyhayer@users.noreply.github.com>
2025-08-16 14:38:05 +0500

    data models and env

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'category_id',
+        'is_active',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
